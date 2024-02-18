5972776784ec74f521f606a8cc50b1ae98150c77
Конпаку <sputnik-999-mel@mail.ru>
2024-02-19 01:52:36 +0500

    main

---
+++ Refactoring/Main.java
@@ -0,0 +1,10 @@
+package Refactoring;
+
+import Refactoring.controller.AppController;
+
+public class Main {
+    public static void main(String[] args) {
+        AppController appController = new AppController();
+        appController.runCalc();
+    }
+}
